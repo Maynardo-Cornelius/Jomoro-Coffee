d5b81d81ba255f6fa3ae1af0aeba1d49473dc529
Krees01 <kristama02@gmail.com>
2026-06-02 08:20:52 +0700

    Use environment variable for JWT secret
    Removed hardcoded JWT secret and used environment variable instead.

--- product-service/src/auth/jwt.strategy.ts
+++ product-service/src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'rahasia_jomoro_super_aman_123',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
